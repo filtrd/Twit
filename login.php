e28a829d10c225f770061f515d301123d7e03cd7
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-01 23:07:44 +0100

    Refactor login to use inc backend

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/inc/database.php';
 
 if (current_user()) { header('Location: index.php'); exit; }
 $error = '';
