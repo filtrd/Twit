1e52ca356ffd95a6694d5d43d9369aa93cd07cad
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-02 21:18:14 +0100

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -99,7 +99,3 @@ function e(string $value): string
 {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
-function e(string $value): string
-{
-    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-}
