f41af9b98b5446a62a0ea3b462afac7bb718278a
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-03 18:48:09 +0100

    Clean up post configuration

--- inc/config.php
+++ inc/config.php
@@ -1,22 +1,21 @@
 <?php
 
+// Site
 // Site name shown across the application.
 $siteName = 'Microblog';
 
 // Short tagline shown below the site name.
 $tagLine = 'Share short thoughts with the world.';
 
-// Base URL used for links and site references.
-$siteUrl = 'http://localhost:8000';
-
+// Posts
 // Maximum length allowed for a post.
 $maxPostLength = 280;
 
 // Maximum number of times a post can be edited.
-$editCount = '2';
+$postEditCount = '2';
 
 // Number of minutes after posting that editing is allowed.
-$editTime = '5';
+$postEditTime = '5';
 
 // Number of minutes after posting that deletion is allowed.
-$deleteTime = '10';
+$postDeleteTime = '10';
