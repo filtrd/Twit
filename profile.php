053d021b1954338614f7dd45b8e54243ddc3f860
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-04 00:57:56 +0100

    Align follow control to far right

--- profile.php
+++ profile.php
@@ -84,7 +84,7 @@ $avatarError = trim($_GET['avatar_error'] ?? '');
                     <p><?= $postCount ?> Posts &middot; <?= $followerCount ?> Followers &middot; <?= $followingCount ?> Following</p>
                 </div>
                 <?php if ($user && (int)$user['id'] !== (int)$profile['id']): ?>
-                    <form class="follow-form" method="post" action="follow.php"><input type="hidden" name="user_id" value="<?= (int)$profile['id'] ?>"><input type="hidden" name="csrf" value="<?= e(csrf_token()) ?>"><button class="button"><?= $isFollowing ? 'Unfollow' : 'Follow' ?></button></form>
+                    <form class="follow-form" method="post" action="follow.php" style="margin-left:auto;"><input type="hidden" name="user_id" value="<?= (int)$profile['id'] ?>"><input type="hidden" name="csrf" value="<?= e(csrf_token()) ?>"><button class="button"><?= $isFollowing ? 'Unfollow' : 'Follow' ?></button></form>
                 <?php endif; ?>
             </div>
 
