c6de66fc3c3c4c0d892edff39500f7ab7b6275c9
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-03 18:12:09 +0100

    Make post edit and delete limits configurable

--- inc/config.php
+++ inc/config.php
@@ -4,3 +4,7 @@ $siteName = 'Microblog';
 $tagLine = 'Share short thoughts with the world.';
 $siteUrl = 'http://localhost:8000';
 $maxPostLength = 280;
+
+$editCount = '2'; // Editable twice
+$editTime = '5'; // 5 minutes
+$deleteTime = '10'; // 10 minutes
