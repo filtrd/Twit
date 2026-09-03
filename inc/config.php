3f6fbefdea72e5b8fba61f5be1dd3eec5312eaa5
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-03 18:29:56 +0100

    Add comments to config variables

--- inc/config.php
+++ inc/config.php
@@ -1,10 +1,22 @@
 <?php
 
+// Site name shown across the application.
 $siteName = 'Microblog';
+
+// Short tagline shown below the site name.
 $tagLine = 'Share short thoughts with the world.';
+
+// Base URL used for links and site references.
 $siteUrl = 'http://localhost:8000';
+
+// Maximum length allowed for a post.
 $maxPostLength = 280;
 
-$editCount = '2'; // Editable twice
-$editTime = '5'; // 5 minutes
-$deleteTime = '10'; // 10 minutes
+// Maximum number of times a post can be edited.
+$editCount = '2';
+
+// Number of minutes after posting that editing is allowed.
+$editTime = '5';
+
+// Number of minutes after posting that deletion is allowed.
+$deleteTime = '10';
