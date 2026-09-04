0c837936ce3336f88c939ae1cc86ab21b8899132
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-04 22:46:26 +0100

    Remove merged JavaScript entry point

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-import { initCommon } from './common.js';
-import { initFeed } from './feed.js';
-import { initComposer } from './composer.js';
-
-const common = initCommon();
-initFeed(common);
-initComposer();
